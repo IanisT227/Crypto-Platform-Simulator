3fbafd15ebd382e7c4fdc04b248fe660640a5d08
Ianis_Teja <ianist2001@gmail.com>
2025-06-17 17:33:53 +0300

    Setup Color palette and generic api responses and errors

--- composeApp/src/commonMain/kotlin/org/ianis_teja/crypto_platform_simulator/App.kt
+++ composeApp/src/commonMain/kotlin/org/ianis_teja/crypto_platform_simulator/App.kt
@@ -1,10 +1,14 @@
 package org.ianis_teja.crypto_platform_simulator
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import org.ianis_teja.crypto_platform_simulator.app.theme.CryptoPlatformTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    CryptoPlatformTheme(darkTheme = isSystemInDarkTheme()) {
 
+    }
 }
---
+++ composeApp/src/commonMain/kotlin/org/ianis_teja/crypto_platform_simulator/app/core/domain/Error.kt
@@ -0,0 +1,4 @@
+package org.ianis_teja.crypto_platform_simulator.app.core.domain
+
+interface Error {
+}
